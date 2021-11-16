7e47e192818acb80535defbff2124cabe85db775
canosaer <67963828+canosaer@users.noreply.github.com>
2021-11-16 15:16:37 -0500

    built-in search

--- wp-content/themes/my-first-theme/header.php
+++ wp-content/themes/my-first-theme/header.php
@@ -26,10 +26,21 @@
                 <figure class="logo">
                     <a class="logo__link" href="<?php echo home_url("/"); ?>"></a>
                 </figure>
-                <form class="search-bar">
+                <!-- <form class="search-bar">
                     <button class="search-bar__button">
                         <img class="search-bar__button-icon" src="http://localhost/wp-content/uploads/2021/11/magnifying-glass.png" alt="search">
                     </button>
                     <input class="search-bar__input" placeholder="Search">
+                </form> -->
+
+                <form role="search" method="get" action="http://localhost/" class="wp-block-search__button-inside wp-block-search__icon-button wp-block-search">
+                    <div class="wp-block-search__inside-wrapper">
+                        <input type="search" id="wp-block-search__input-1" class="wp-block-search__input" name="s" value="" placeholder=""  required />
+                        <button type="submit" class="wp-block-search__button has-icon">
+                            <svg id="search-icon" class="search-icon" viewBox="0 0 24 24" width="24" height="24">
+			                    <path d="M13.5 6C10.5 6 8 8.5 8 11.5c0 1.1.3 2.1.9 3l-3.4 3 1 1.1 3.4-2.9c1 .9 2.2 1.4 3.6 1.4 3 0 5.5-2.5 5.5-5.5C19 8.5 16.5 6 13.5 6zm0 9.5c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z"></path>
+			                </svg>
+                        </button>
+                    </div>
                 </form>
             </header>
