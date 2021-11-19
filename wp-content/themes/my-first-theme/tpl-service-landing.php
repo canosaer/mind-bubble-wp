a06c07a92b3870ec44e0e172b2977665dc6aa537
canosaer <67963828+canosaer@users.noreply.github.com>
2021-11-19 15:21:34 -0500

    hw unstyled

--- wp-content/themes/my-first-theme/tpl-service-landing.php
+++ wp-content/themes/my-first-theme/tpl-service-landing.php
@@ -7,28 +7,45 @@
 get_header();
 ?>
 
-<?php
-    $serviceLandingArgs = array();
-    $serviceLandingQ = new WP_Query( $serviceLandingArgs );
-    if($serviceLandingQ->have_posts()){
+<section class="service-landing">
+    <h1 class="service-landing__heading">Services</h1>
+    <div class="service-landing__gallery">
+
+    <?php
+        $serviceLandingArgs = array(
+            'post_type' => 'service',
+            'posts_per_page' => -1,
+            'orderby' => 'title',
+            'order' => 'asc'
+        );
+
+        $serviceLandingQ = new WP_Query( $serviceLandingArgs );
+        if($serviceLandingQ->have_posts()){
         ?>
-        <ul class="example">
-        <?php
+            <?php
+                while ($serviceLandingQ->have_posts()){
+                    $serviceLandingQ->the_post();
+                    ?>
+                        <a class="card" href="<?php echo get_the_permalink(); ?>">
+                            <img class="card__image" src="<?php the_field('icon'); ?>">
+                            <div class="card__text-display">
+                                <h2 class="card__heading"> <?php the_field('service_name'); ?> </h2>
+                            </div>
+					    </a>
+                    <?php
+                }
+            ?>
+            </ul>
+            <?php
+        }
+        wp_reset_postdata();
+    ?>
+
+    </div>
+
+</section>
 
 
-            while ($serviceLandingQ->have_posts()){
-                $serviceLandingQ->the_post();
-                ?>
-
-
-                <?php
-            }
-        ?>
-        </ul>
-        <?php
-    }
-    wp_reset_postdata();
-?>
 
 <?php
 get_footer();
--- wp-content/themes/my-first-theme/tpl-team-member-landing-page.php
+++ wp-content/themes/my-first-theme/tpl-team-member-landing-page.php
@@ -7,15 +7,45 @@
 get_header();
 ?>
 
-<?php
-while (have_posts()) : the_post();
-?>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
+<section class="team-landing">
+    <h1 class="team-landing__heading">Team</h1>
+    <div class="team-landing__gallery">
 
-<?php
-endwhile;
-?>
+    <?php
+        $teamLandingArgs = array(
+            'post_type' => 'team_member',
+            'posts_per_page' => -1,
+            'meta_key' => 'last_name',
+            'orderby' => 'menu_order meta_value',
+            'order' => 'asc'
+        );
+
+        $teamLandingQ = new WP_Query( $teamLandingArgs );
+        if($teamLandingQ->have_posts()){
+        ?>
+            <?php
+                while ($teamLandingQ->have_posts()){
+                    $teamLandingQ->the_post();
+                    ?>
+                        <a class="card" href="<?php echo get_the_permalink(); ?>">
+                            <img class="card__image" src="<?php the_field('headshot'); ?>">
+                            <div class="card__text-display">
+                                <h2 class="card__heading"> <?php the_field('name'); ?> </h2>
+                                <h3 class="card__subheading"> <?php the_field('job_title'); ?> </h3>
+                            </div>
+					    </a>
+                    <?php
+                }
+            ?>
+            </ul>
+            <?php
+        }
+        wp_reset_postdata();
+    ?>
+
+    </div>
+
+</section>
 
 <?php
 get_footer();
