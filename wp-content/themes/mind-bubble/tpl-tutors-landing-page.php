2fb36e09d9a20cf2407ff5f3b457d91ce7edb5eb
canosaer <67963828+canosaer@users.noreply.github.com>
2021-12-11 20:30:08 -0500

    more

--- wp-content/themes/mind-bubble/tpl-tutors-landing-page.php
+++ wp-content/themes/mind-bubble/tpl-tutors-landing-page.php
@@ -8,8 +8,10 @@ get_header();
 
 <section class="tutor-hero">
     <img class="tutor-hero__image" src="<?php the_field('hero_image'); ?>" alt="tutoring hero image"></img>
-    <p class="tutor-hero__text"><?php the_field('hero_text'); ?></p>
-    <a class="tutor-hero__button" href="<?php the_field('tutoring_sign_up_link'); ?>"><?php the_field('button_text'); ?></a>
+    <div class="tutor-hero__content">
+        <p class="tutor-hero__text"><?php the_field('hero_text'); ?></p>
+        <a class="tutor-hero__button" href="<?php the_field('tutoring_sign_up_link'); ?>"><?php the_field('button_text'); ?></a>
+    </div>
 </section>
 
 <section class="tutor-gallery">
@@ -18,26 +20,25 @@ get_header();
 
     <div class="tutor-gallery__card-display">
         <?php
-            $teamLandingArgs = array(
-                'post_type' => 'team_member',
+            $tutorsLandingArgs = array(
+                'post_type' => 'tutors',
                 'posts_per_page' => -1,
-                'meta_key' => 'last_name',
-                'orderby' => 'menu_order meta_value',
+                'orderby' => 'title',
                 'order' => 'asc'
             );
 
-            $teamLandingQ = new WP_Query( $teamLandingArgs );
-            if($teamLandingQ->have_posts()){
+            $tutorsLandingQ = new WP_Query( $tutorsLandingArgs );
+            if($tutorsLandingQ->have_posts()){
             ?>
                 <?php
-                    while ($teamLandingQ->have_posts()){
-                        $teamLandingQ->the_post();
+                    while ($tutorsLandingQ->have_posts()){
+                        $tutorsLandingQ->the_post();
                         ?>
-                            <a class="card" href="<?php echo get_the_permalink(); ?>">
-                                <img class="card__image" src="<?php the_field('headshot'); ?>">
-                                <div class="card__text-display">
-                                    <h2 class="card__heading"> <?php the_field('name'); ?> </h2>
-                                    <h3 class="card__subheading"> <?php the_field('job_title'); ?> </h3>
+                            <a class="tutor-card" href="<?php echo get_the_permalink(); ?>">
+                                <img class="tutor-card__image" src="<?php the_field('headshot'); ?>">
+                                <div class="tutor-card__text-display">
+                                    <h2 class="tutor-card__name"> <?php the_field('first_name'); ?> </h2>
+                                    <p class="tutor-card__detail">With Mind Bubble since <?php the_field('with_mind_bubble_since'); ?></p>
                                 </div>
                             </a>
                         <?php
@@ -56,7 +57,7 @@ get_header();
 
 <section class="get-help">
     <h3 class="get-help__text">Not sure where to begin? We can help.</h3>
-    <a class="get-help__button" href="mailto:<?php the_field('more_info_email_address'); ?>">Get More Info</a>
+    <a class="get-help__button" href="mailto:<?php the_field('more_info_email_address'); ?>">Contact Us</a>
 </section>
 
 <?php
